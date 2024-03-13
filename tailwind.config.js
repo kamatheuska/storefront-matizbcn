2351669e87a50d67cc55081c914bb7e3be9bf437
Nicolas Ramirez <nicolas@matizlab.com>
2024-03-13 22:08:37 +0100

    add footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
     content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
     theme: {
-        extend: {},
+        extend: {
+            boxShadow: {
+                input: "rgb(18, 18, 18) 0px 0px 0px 2px",
+            },
+        },
         fontFamily: {
             sans: ["'Work Sans'"],
         },
