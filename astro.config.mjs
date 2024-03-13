379753a56da497a28ce192d92d0cb4d690e322cf
Nicolas Ramirez <nicolas@matizlab.com>
2024-03-13 23:01:27 +0100

    add metadescription, robot.txt and sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,15 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import robotsTxt from "astro-robots-txt";
+import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
+    site: "https://storefront-matizbcn.pages.dev",
     integrations: [
         tailwind({
             applyBaseStyles: false,
         }),
+        robotsTxt(),
+        sitemap(),
     ],
 });
--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,6 @@
 export type PageMetadata = {
     title: string;
+    description: string;
 };
 
 export type ImageProps = {
